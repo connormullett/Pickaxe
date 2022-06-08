61007445a9ee813acbb7b4f68e9085677ae93dc4
connor mullett <connormullett@gmail.com>
2022-06-08 02:24:10 -0400

    constructing blockheader; create merkle root; serialize txs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use std::{net::SocketAddr, str::FromStr};
+use std::{
+    net::SocketAddr,
+    str::FromStr,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
-use bitcoin::{OutPoint, Script, Transaction, TxIn, TxOut};
+use bitcoin::{
+    consensus::{deserialize, serialize},
+    hashes::hex::FromHex,
+    BlockHash, BlockHeader, OutPoint, Script, Transaction, TxIn, TxMerkleNode, TxOut,
+};
 use clap::Parser;
 use jsonrpc::{arg, Client};
+use rs_merkle::{algorithms::Sha256, Hasher, MerkleTree};
 use serde::{Deserialize, Serialize};
 
 #[derive(Parser)]
@@ -47,7 +56,7 @@ pub struct GetBlockTemplateReturn {
     previousblockhash: String,
     transactions: Vec<Tx>,
     coinbaseaux: Option<CoinbaseAuxValues>,
-    coinbasevalue: i32,
+    coinbasevalue: u64,
     longpollid: String,
     target: String,
     mintime: i128,
@@ -58,7 +67,7 @@ pub struct GetBlockTemplateReturn {
     weightlimit: i32,
     curtime: i128,
     bits: String,
-    height: i32,
+    height: u64,
     default_witness_commitment: String,
 }
 
@@ -86,14 +95,51 @@ async fn main() -> anyhow::Result<()> {
 
     let template: GetBlockTemplateReturn = response.result().unwrap();
 
-    println!("{:#?}", template.transactions.get(0).unwrap());
+    let pub_key_hash = String::from("93ce48570b55c42c2af816aeaba06cfee1224fae");
+    let mut transactions =
+        vec![create_coinbase(pub_key_hash, template.height, template.coinbasevalue).await];
+
+    let mut transaction_data: Vec<Transaction> = template
+        .transactions
+        .iter()
+        .map(|tx| {
+            let tx_hex = Vec::from_hex(&tx.data).unwrap();
+            let transaction = deserialize(&tx_hex).unwrap();
+            transaction
+        })
+        .collect();
+
+    transactions.append(&mut transaction_data);
+
+    let leaves: Vec<[u8; 32]> = transactions
+        .iter()
+        .map(|x| Sha256::hash(&serialize(x)))
+        .collect();
+
+    let tree = MerkleTree::<Sha256>::from_leaves(&leaves);
+    let root = tree.root().unwrap();
+
+    let start = SystemTime::now();
+    let time = start
+        .duration_since(UNIX_EPOCH)
+        .expect("time went backwards")
+        .as_secs();
+
+    let header = BlockHeader {
+        version: template.version,
+        prev_blockhash: BlockHash::from_hex(&template.previousblockhash).unwrap(),
+        merkle_root: TxMerkleNode::from_hex(&hex::encode(root)).unwrap(),
+        time: time.try_into().unwrap(),
+        bits: template.bits,
+        nonce: todo!(),
+    };
 
     Ok(())
 }
 
 pub async fn create_coinbase(
     public_key_hash: String,
-    block_height: i32,
+    block_height: u64,
     value: u64,
 ) -> Transaction {
     let outpoint = OutPoint::null();
